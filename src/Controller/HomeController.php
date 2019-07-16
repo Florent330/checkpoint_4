5cb29915cfb300fbbd353cad6bab21a756cd4378
Duval Florent <Florent_Duval_student2019@wilder.school>
2019-07-16 20:30:25 +0200

    affichage show et crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Show;
+use App\Repository\ShowRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -12,11 +15,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
-    {
+    public function index()    {
+
+        $showsRepo = $this->getDoctrine()->getManager()->getRepository(Show::class);
+        $shows = $showsRepo->findBy([],['id'=> 'DESC'], 5, 0);
+
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'show' => $shows,
+
         ]);
     }
+
+
 }
---
+++ src/Form/ShowType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Show;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ShowType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('name')
+            ->add('description')
+            ->add('picture')
+            ->add('artistes', null, ['choice_label'=> 'lastname',
+                'expanded' => true,
+                'multiple' => true,
+                'by_reference' => false]
+                )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Show::class,
+        ]);
+    }
+}
